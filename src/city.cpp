eb572e4c57d27942ac1b7ec2114f8cb7d873ccfe
Ashley Raynal <brown2a@mit.edu>
2019-01-19 23:29:35 -0500

    Remove console when leaving program.

--- src/city.cpp
+++ src/city.cpp
@@ -1,8 +1,15 @@
 #include "Game.h"
+#include <iostream>
+
+#include "windows.h"
 
 int
-main()
+main(int argc, wchar_t* argv[])
 {
 	city::Game().loop();
+	std::cerr << "Well done, bye bye\n";
+
+	// Manually free console
+	FreeConsole();
 	return 0;
 }
