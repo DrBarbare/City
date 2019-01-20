57e879dbc44a7dec501bb1dd94e160b270e8752c
Ashley Raynal <brown2a@mit.edu>
2019-01-19 23:40:58 -0500

    Trying better handling of console.

--- src/city.cpp
+++ src/city.cpp
@@ -1,13 +1,11 @@
-#include "Game.h"
-#include <iostream>
+#include <windows.h>
 
-#include "windows.h"
+#include "Game.h"
 
 int
 main(int argc, wchar_t* argv[])
 {
 	city::Game().loop();
-	std::cerr << "Well done, bye bye\n";
 
 	// Manually free console
 	FreeConsole();
